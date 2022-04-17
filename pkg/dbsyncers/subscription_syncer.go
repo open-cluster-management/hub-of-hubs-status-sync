4544d43227f31fe6cbb0df64852d9ccec3be1557
Maroon Ayoub <maroon.ayoub@ibm.com>
2022-04-17 13:25:59 +0300

    added generic db syncer
    Signed-off-by: Maroon Ayoub <maroon.ayoub@ibm.com>

--- pkg/dbsyncers/subscription_syncer.go
+++ pkg/dbsyncers/subscription_syncer.go
@@ -23,11 +23,14 @@ const (
 )
 
 func addSubscriptionDBSyncer(mgr ctrl.Manager, databaseConnectionPool *pgxpool.Pool, syncInterval time.Duration) error {
-	err := mgr.Add(&subscriptionDBSyncer{
-		client:                 mgr.GetClient(),
-		log:                    ctrl.Log.WithName("subscriptions-db-syncer"),
-		databaseConnectionPool: databaseConnectionPool,
-		syncInterval:           syncInterval,
+	err := mgr.Add(&genericDBSyncer{
+		syncInterval: syncInterval,
+		syncFunc: func(ctx context.Context) {
+			syncSubscriptions(ctx,
+				ctrl.Log.WithName("subscriptions-db-syncer"),
+				databaseConnectionPool,
+				mgr.GetClient())
+		},
 	})
 	if err != nil {
 		return fmt.Errorf("failed to add subscriptions status syncer to the manager: %w", err)
@@ -36,65 +39,15 @@ func addSubscriptionDBSyncer(mgr ctrl.Manager, databaseConnectionPool *pgxpool.P
 	return nil
 }
 
-type subscriptionDBSyncer struct {
-	client                 client.Client
-	log                    logr.Logger
-	databaseConnectionPool *pgxpool.Pool
-	syncInterval           time.Duration
-}
+func syncSubscriptions(ctx context.Context, log logr.Logger, databaseConnectionPool *pgxpool.Pool,
+	k8sClient client.Client) {
+	log.Info("performing sync of subscription status")
 
-func (syncer *subscriptionDBSyncer) Start(ctx context.Context) error {
-	ctxWithCancel, cancelContext := context.WithCancel(ctx)
-	defer cancelContext()
-
-	go syncer.periodicSync(ctxWithCancel)
-
-	<-ctx.Done() // blocking wait for stop event
-	syncer.log.Info("stop performing sync")
-
-	return nil // context cancel is called before exiting this function
-}
-
-func (syncer *subscriptionDBSyncer) periodicSync(ctx context.Context) {
-	ticker := time.NewTicker(syncer.syncInterval)
-
-	var (
-		cancelFunc     context.CancelFunc
-		ctxWithTimeout context.Context
-	)
-
-	for {
-		select {
-		case <-ctx.Done(): // we have received a signal to stop
-			ticker.Stop()
-
-			if cancelFunc != nil {
-				cancelFunc()
-			}
-
-			return
-
-		case <-ticker.C:
-			// cancel the operation of the previous tick
-			if cancelFunc != nil {
-				cancelFunc()
-			}
-
-			ctxWithTimeout, cancelFunc = context.WithTimeout(ctx, syncer.syncInterval)
-
-			syncer.sync(ctxWithTimeout)
-		}
-	}
-}
-
-func (syncer *subscriptionDBSyncer) sync(ctx context.Context) {
-	syncer.log.Info("performing sync of subscription status")
-
-	rows, err := syncer.databaseConnectionPool.Query(ctx,
+	rows, err := databaseConnectionPool.Query(ctx,
 		fmt.Sprintf(`SELECT id, payload->'metadata'->>'name', payload->'metadata'->>'namespace' 
 		FROM spec.%s WHERE deleted = FALSE`, subscriptionsSpecTableName))
 	if err != nil {
-		syncer.log.Error(err, "error in getting subscriptions spec")
+		log.Error(err, "error in getting subscriptions spec")
 		return
 	}
 
@@ -103,40 +56,41 @@ func (syncer *subscriptionDBSyncer) sync(ctx context.Context) {
 
 		err := rows.Scan(&id, &name, &namespace)
 		if err != nil {
-			syncer.log.Error(err, "error in select", "table", subscriptionsSpecTableName)
+			log.Error(err, "error in select", "table", subscriptionsSpecTableName)
 			continue
 		}
 
 		instance := &appsv1.Subscription{}
-		err = syncer.client.Get(ctx, client.ObjectKey{Name: name, Namespace: namespace}, instance)
 
+		err = k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: namespace}, instance)
 		if err != nil {
-			syncer.log.Error(err, "error in getting CR", "name", name, "namespace", namespace)
+			log.Error(err, "error in getting CR", "name", name, "namespace", namespace)
 			continue
 		}
 
-		go syncer.handleSubscription(ctx, instance)
+		go handleSubscription(ctx, log, databaseConnectionPool, k8sClient, instance)
 	}
 }
 
-func (syncer *subscriptionDBSyncer) handleSubscription(ctx context.Context, subscription *appsv1.Subscription) {
-	syncer.log.Info("handling a subscription", "uid", subscription.GetUID())
+func handleSubscription(ctx context.Context, log logr.Logger, databaseConnectionPool *pgxpool.Pool,
+	k8sClient client.StatusClient, subscription *appsv1.Subscription) {
+	log.Info("handling a subscription", "uid", subscription.GetUID())
 
-	subscriptionStatuses, err := syncer.getSubscriptionStatus(ctx, subscription)
+	subscriptionStatuses, err := getSubscriptionStatus(ctx, databaseConnectionPool, subscription)
 	if err != nil {
-		syncer.log.Error(err, "failed to get status of a subscription", "uid", subscription.GetUID())
+		log.Error(err, "failed to get status of a subscription", "uid", subscription.GetUID())
 		return
 	}
 
-	if err = syncer.updateSubscriptionStatus(ctx, subscription, subscriptionStatuses); err != nil {
-		syncer.log.Error(err, "failed to update subscription status")
+	if err = updateSubscriptionStatus(ctx, k8sClient, subscription, subscriptionStatuses); err != nil {
+		log.Error(err, "failed to update subscription status")
 	}
 }
 
 // returns array of CompliancePerClusterStatus and error.
-func (syncer *subscriptionDBSyncer) getSubscriptionStatus(ctx context.Context,
+func getSubscriptionStatus(ctx context.Context, databaseConnectionPool *pgxpool.Pool,
 	subscription *appsv1.Subscription) (*appsv1.SubscriptionClusterStatusMap, error) {
-	rows, err := syncer.databaseConnectionPool.Query(ctx,
+	rows, err := databaseConnectionPool.Query(ctx,
 		fmt.Sprintf(`SELECT leaf_hub_name, payload->'status'->'statuses' FROM status.%s
 			WHERE payload->'metadata'->>'name'=$1 AND payload->'metadata'->>'namespace'=$2`,
 			subscriptionsStatusTableName), subscription.Name, subscription.Namespace)
@@ -158,14 +112,14 @@ func (syncer *subscriptionDBSyncer) getSubscriptionStatus(ctx context.Context,
 
 		// each leaf hub has only 1 unique subscription deployment (name, ns)
 		for clusterName, subscriptionPerClusterStatus := range leafHubSubscriptionStatuses {
-			subscriptionStatuses[clusterName] = subscriptionPerClusterStatus
+			subscriptionStatuses[fmt.Sprintf("%s-%s", leafHubName, clusterName)] = subscriptionPerClusterStatus
 		}
 	}
 
 	return &subscriptionStatuses, nil
 }
 
-func (syncer *subscriptionDBSyncer) updateSubscriptionStatus(ctx context.Context, subscription *appsv1.Subscription,
+func updateSubscriptionStatus(ctx context.Context, k8sClient client.StatusClient, subscription *appsv1.Subscription,
 	subscriptionStatuses *appsv1.SubscriptionClusterStatusMap) error {
 	originalSubscription := subscription.DeepCopy()
 
@@ -176,7 +130,7 @@ func (syncer *subscriptionDBSyncer) updateSubscriptionStatus(ctx context.Context
 		subscription.Status.Reason = statusPropagatedFromLeafHubs
 	}
 
-	err := syncer.client.Status().Patch(ctx, subscription, client.MergeFrom(originalSubscription))
+	err := k8sClient.Status().Patch(ctx, subscription, client.MergeFrom(originalSubscription))
 	if err != nil && !errors.IsNotFound(err) {
 		return fmt.Errorf("failed to update subscription CR: %w", err)
 	}
